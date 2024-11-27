df9ea537843101547606602128514715224eb4b2
usiqwerty <benlogin1@protonmail.com>
2024-11-27 20:30:59 +0500

    Style changes

--- src/widgets/Editor.tsx
+++ src/widgets/Editor.tsx
@@ -8,16 +8,20 @@ export default function Editor({running, setTime, defaultTime, setDefaultTime}: 
         setDefaultTime(60 * minutes + seconds);
     }, [minutes, seconds]);
     return <div className={"ui editor" + (running ? " invisible" : "")}>
-        <input type={"number"} pattern={"[0-9]*"}
-               className={"input"}
-               onChange={e => setMinutes(Number.parseInt(e.target.value))}
-               placeholder={"minutes"}/>
-        :
-        <input type={"number"} pattern={"[0-9]*"}
-               className={"input"}
-               onChange={e => setSeconds(Number.parseInt(e.target.value))}
-               placeholder={"seconds"}/>
-        Here will be an editor
-        <button className={"btn"} onClick={() => setTime(defaultTime)}>Reset</button>
+        <span id={"title"}>timed</span>
+        <span className={"editor-row"}>
+            <input type={"number"} pattern={"[0-9]*"}
+                   className={"input"}
+                   onChange={e => setMinutes(Number.parseInt(e.target.value))}
+                   placeholder={"minutes"}/>
+            :
+            <input type={"number"} pattern={"[0-9]*"}
+                   className={"input"}
+                   onChange={e => setSeconds(Number.parseInt(e.target.value))}
+                   placeholder={"seconds"}/>
+            <button className={"btn"} onClick={() => setTime(defaultTime)}>Reset</button>
+        </span>
+
+
     </div>;
 }
